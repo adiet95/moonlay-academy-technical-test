dc93c1b1f58681f5d282067b1777cc831a458cb6
Adiet Alimudin <55053994+adiet95@users.noreply.github.com>
2022-11-02 12:35:44 +0700

    add validate

--- src/libs/validate.go
+++ src/libs/validate.go
@@ -15,7 +15,7 @@ func Validation(title, desc string) error {
 		return errors.New("title must be alphanumeric")
 	}
 
-	if len(desc) > 1000 && len(desc) < 0 {
+	if len(desc) > 1000 && len(desc) <= 0 {
 		return errors.New("title must be greater than nil character & less than 1000")
 	}
 	return nil
